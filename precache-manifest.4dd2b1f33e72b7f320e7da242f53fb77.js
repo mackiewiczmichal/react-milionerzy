7680d3fd4654737642a41fce295c74f7b4486d9e
mackiewiczmichal <threemz69@gmail.com>
2020-03-29 22:30:43 +0200

    Updates

---
+++ precache-manifest.4dd2b1f33e72b7f320e7da242f53fb77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad225404aea35ad85e427b23f69b851",
+    "url": "/react-milionerzy/index.html"
+  },
+  {
+    "revision": "0e0c9b2ba5ed3f736f96",
+    "url": "/react-milionerzy/static/css/main.56889f46.chunk.css"
+  },
+  {
+    "revision": "8d6dcb565a5142e3a9bf",
+    "url": "/react-milionerzy/static/js/2.8caedb70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milionerzy/static/js/2.8caedb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0c9b2ba5ed3f736f96",
+    "url": "/react-milionerzy/static/js/main.203a0f43.chunk.js"
+  },
+  {
+    "revision": "d65b48eecb42cdd08025",
+    "url": "/react-milionerzy/static/js/runtime-main.e091b4f3.js"
+  },
+  {
+    "revision": "0dd5fc4831f3029ca463f6054ac1f34d",
+    "url": "/react-milionerzy/static/media/background.0dd5fc48.jpg"
+  }
+]);
