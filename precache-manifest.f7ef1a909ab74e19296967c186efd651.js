4a4aef5e281a6601f7060a0f65aa7aaad62c335a
mackiewiczmichal <threemz69@gmail.com>
2020-06-05 10:18:13 +0200

    Updates

--- precache-manifest.f7ef1a909ab74e19296967c186efd651.js
+++ precache-manifest.f7ef1a909ab74e19296967c186efd651.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8261c0d32e5431bc3bc02e4595c61522",
+    "revision": "48c67b923ace5bae9aaa4772541adad0",
     "url": "/react-milionerzy/index.html"
   },
   {
-    "revision": "29ee65db6145873089ce",
+    "revision": "681132661a97142117fe",
     "url": "/react-milionerzy/static/css/main.e952462f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-milionerzy/static/js/2.1df84219.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "29ee65db6145873089ce",
-    "url": "/react-milionerzy/static/js/main.04ce1d38.chunk.js"
+    "revision": "681132661a97142117fe",
+    "url": "/react-milionerzy/static/js/main.d9d18a2d.chunk.js"
   },
   {
     "revision": "d65b48eecb42cdd08025",
@@ -26,5 +26,17 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "0dd5fc4831f3029ca463f6054ac1f34d",
     "url": "/react-milionerzy/static/media/background.0dd5fc48.jpg"
+  },
+  {
+    "revision": "d27f45adf88f4e70e787423e4db695ad",
+    "url": "/react-milionerzy/static/media/correct_answer.d27f45ad.mp3"
+  },
+  {
+    "revision": "3d8ba0a6804c784263ebab21dc1f4027",
+    "url": "/react-milionerzy/static/media/lets_play.3d8ba0a6.mp3"
+  },
+  {
+    "revision": "b3f5ba3c07400794a79c08f0443ca7b8",
+    "url": "/react-milionerzy/static/media/main_theme.b3f5ba3c.mp3"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-milionerzy/precache-manifest.e92587ec04821b183a2e2d05e5d48d1b.js"
+  "/react-milionerzy/precache-manifest.f7ef1a909ab74e19296967c186efd651.js"
 );
 
 self.addEventListener('message', (event) => {
